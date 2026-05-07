ec247ccfa30dbf354d513df285de0d572b086181
gabrielsartorio <gabriel.sartorio@sou.unifeob.edu.br>
2026-05-06 21:50:43 -0300

    Update filme_controller.py

--- src/controller/filme_controller.py
+++ src/controller/filme_controller.py
@@ -1,1 +1,15 @@
+from src.service.filme_service import FilmeService
 
+class FilmeController:
+    def __init__(self):
+        self.service = FilmeService()
+
+    def adicionar_filme(self, titulo, genero, duracao, diretor, elenco):
+        try:
+            fid = self.service.cadastrar_filme(titulo, genero, duracao, diretor, elenco)
+            return f"Sucesso! Filme salvo com ID {fid}"
+        except Exception as e:
+            return f"Erro: {e}"
+
+    def listar(self):
+        return self.service.listar_filmes()
